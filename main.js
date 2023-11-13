d33de7241c8b93d41521776c4ce679dcc099d731
rajk123-code <149297400+rajk123-code@users.noreply.github.com>
2023-11-13 09:42:32 +0530

    added my useful file

---
+++ main.js
@@ -0,0 +1,17 @@
+console.log('Hello World!');
+
+let m = 1;
+switch(m){
+  case 1:
+    console.log('mt');
+    // code
+    break;
+  case 2:
+    console.log('gt');
+    // code
+    break;
+  
+}
+let age = 42;
+let isadult = (age = 18) ? 'to young': 'old enough'
+console.log(isadult);
